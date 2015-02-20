b1f924fd09cbe6eb98d2642e60e0facc06ae3b28
goldcode101 <chris_lnz@yahoo.com>
2015-02-19 20:14:28 -0500

    Starting files

---
+++ WhereDoesItAllGo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WhereDoesItAllGo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Dashboard()
+        {
+            if (Session["UserID"] == null) return RedirectToAction("Login");
+            int userId = Convert.ToInt32(Session["UserID"]);
+            //get the user by userId and load their page
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            if (Session["UserID"] != null) return RedirectToAction("Dashboard");
+
+
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WhereDoesItAllGo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WhereDoesItAllGo.MvcApplication" Language="C#" %>
